05aa207a0fe999bed01a0878ef53b5962efc3fd3
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-08 17:34:02 -0300

    Links externos agora sao dinamicos

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,5 +1,11 @@
 import styles from './sticky-footer.module.scss'
 
+const links = [
+  { href: 'https://dev.thiagosilva.cc/', label: 'Thiago Silva' },
+  { href: 'https://github.com/silva-thiago', label: 'GitHub' },
+  { href: 'https://www.linkedin.com/in/tjlsilva', label: 'LinkedIn' },
+]
+
 const Footer = () => {
   return (
     <React.Fragment>
@@ -7,11 +13,15 @@ const Footer = () => {
         <div className={'grid grid-cols-1 bg-gray-900 p-16'}>
           <div className={'flow-root'}>
             <div className={'block text-xl font-black text-white text-center px-4 py-2 m-2'}>
-              <p>Desenvolvido por {' '}
-                <a href="https://dev.thiagosilva.cc/" target='blank_' className={'hover:text-gray-400'}>Thiago Silva</a> {' '}
-                {/* <a href="https://github.com/silva-thiago" target='blank_' className={'hover:text-gray-400'}>GitHub</a> | {' '} */}
-                {/* <a href="https://www.linkedin.com/in/tjlsilva/" target='blank_' className={'hover:text-gray-400'}>LinkedIn</a> {' '} */}
-              </p>
+              <ul className={'justify-between items-center space-x-4'}>
+                <span>Desenvolvido por {' '}
+                  {links.map(({ href, label }) => (
+                    <li className={''} key={`${href}${label}`}>
+                      <a href={href} target='blank_' className={'text-white hover:text-gray-400 no-underline'}>{label}</a> {' '}
+                    </li>
+                  ))}
+                </span>
+              </ul>
             </div>
           </div>
           <div className={'flex inline mx-auto'}>
