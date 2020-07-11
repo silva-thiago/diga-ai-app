6d4d0c99550707aabfa6185ae98f90fe7464f330
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-11 14:58:23 -0300

    Definido a comunicação entre a aplicação e a planilha + feddback ao usuário e outras melhorias

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,36 @@
 import Link from 'next/link'
+import useSWR from 'swr'
 
 import Layout from '../components/layout'
 import Card from '../components/card'
 
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
 const Index = () => {
+  const { data, error } = useSWR('/api/get-promo', fetcher)
+
   return (
     <React.Fragment>
       <Layout>
         <Card>
           <div className={'text-center rounded-lg overflow-hidden'}>
-            <img className={'rounded-lg'} src='/img/card-top.jpg' alt='Sunset in the mountains' />
+            <img className={'rounded-lg'} src='/img/megafone.png' alt='Sunset in the mountains' />
             <div className={'py-4'}>
-              <p className={'text-gray-900 text-base'}>Nosso estabelicimento sempre busca por atender melhor seus clientes. Por isso, estamos sempre abertos a ouvir o que vocẽ pensa.</p>
+              <p className={'text-gray-900 text-lg'}>Nosso estabelecimento constantemente busca atender melhor seus clientes. Por isso, estamos sempre abertos a ouvir o que você pensa.</p>
             </div>
             <div className={'py-4'}>
-              <Link href='/pesquisa'>
+              {data && !data.promotionStatus && <Link href='/'>
+                <a className={'font-bold uppercase bg-red-600 text-white hover:text-gray-100 hover:bg-red-700 hover:border-red-700 block rounded-full inline-block shadow px-4 opacity-50 cursor-not-allowed py-2'}>Opine agora</a>
+              </Link>}
+              {data && data.promotionStatus && <Link href='/pesquisa'>
                 <a className={'font-bold uppercase bg-red-600 text-white hover:text-gray-100 hover:bg-red-700 hover:border-red-700 block rounded-full inline-block shadow px-4 py-2'}>Opine agora</a>
-              </Link>
+              </Link>}
             </div>
             <div className={'pt-4'}>
-              <p className={'text-gray-900 text-base'}>Ao dar sua opnião e/ou sugestão, ganhe 10% de desconto na sua próxima compra.</p>
-              <p className={'text-gray-500 text-base pt-4'}>Texto dinâmico vindo da planilha</p>
+              {/* <p className={'text-gray-600 text-base uppercase'}>Atenção!</p> */}
+              {!data && <p className={'text-gray-600 text-base'}>Carregando...</p>}
+              {!error && data && !data.promotionStatus && <p className={'text-gray-600 text-base'}>{data.noPromotionMessage}</p>}
+              {!error && data && data.promotionStatus && <p className={'text-gray-600 text-base'}>{data.promotionMessage}</p>}
             </div>
           </div>
         </Card>
