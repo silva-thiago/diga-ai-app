398dcec73e405f64eac53863b8e272183bc55d50
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-11 14:50:27 -0300

    Melhorias estéticas aplicadas

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,25 +2,25 @@ import styles from './sticky-footer.module.scss'
 
 const links = [
   { href: 'https://dev.thiagosilva.cc/', label: 'Thiago Silva' },
-  { href: 'https://github.com/silva-thiago', label: 'GitHub' },
-  { href: 'https://www.linkedin.com/in/tjlsilva', label: 'LinkedIn' },
+  // { href: 'https://github.com/silva-thiago', label: 'GitHub' },
+  // { href: 'https://www.linkedin.com/in/tjlsilva', label: 'LinkedIn' },
 ]
 
 const Footer = () => {
   return (
     <React.Fragment>
-      <div className={styles.footerContainer}>
-        <div className={'grid grid-cols-1 bg-gray-900 p-16'}>
+      <div className={styles.wrapperFooter}>
+        <div className={'grid grid-cols-1 bg-blue-900 p-16'}>
           <div className={'flow-root'}>
-            <div className={'block text-xl font-black text-white text-center px-4 py-2 m-2'}>
+            <div className={'block font-black text-sm text-white text-center px-4 py-2 m-2'}>
               <ul className={'justify-between items-center space-x-4'}>
-                <span>Desenvolvido por {' '}
-                  {links.map(({ href, label }) => (
-                    <li className={''} key={`${href}${label}`}>
-                      <a href={href} target='blank_' className={'text-white hover:text-gray-400 no-underline'}>{label}</a> {' '}
-                    </li>
-                  ))}
-                </span>
+                {links.map(({ href, label }) => (
+                  <li className={''} key={`${href}${label}`}>
+                    <span>Desenvolvido por
+                      <a href={href} target='blank_' className={'text-white hover:text-gray-400 no-underline'}> {label}</a> {' '}
+                    </span>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           </Link>
         </div>
         <div className={'block lg:hidden'}>
-          <button className={'flex items-center text-gray-900 border border-gray-900 hover:text-red-600 hover:border-red-600 rounded px-3 py-2'}>
+          <button className={'flex items-center text-red-600 border border-red-600 hover:text-white hover:border-transparent hover:bg-red-600 rounded px-3 py-2'}>
             <svg className={'fill-current h-3 w-3'} viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
               <title>Menu</title>
               <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
