deebc41e461ad53975cec79bf07f8137d55a2b0c
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-12 20:51:02 -0300

    Foi adicionado título da página + métodos renomeados

--- pages/pesquisa.jsx
+++ pages/pesquisa.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 
+import PageTitle from '../components'
 import Layout from '../components/layout'
 import Form from '../components/form'
 
@@ -16,7 +17,7 @@ const Pesquisa = () => {
   const notas = [0, 1, 2, 3, 4, 5]
   const [success, setSuccess] = useState(false)
   const [feedback, setFeedback] = useState({})
-  const send = async () => {
+  const sendData = async () => {
     try {
       const response = await fetch('/api/saving-data', {
         method: 'POST',
@@ -25,10 +26,9 @@ const Pesquisa = () => {
       const data = await response.json()
       setSuccess(true)
       setFeedback(data)
-    } catch (error) {}
+    } catch (error) { }
   }
-
-  const onChange = evt => {
+  const handleChange = (evt) => {
     const value = evt.target.value
     const key = evt.target.name
 
@@ -40,31 +40,28 @@ const Pesquisa = () => {
 
   return (
     <React.Fragment>
+      <PageTitle title='Pesquisa - ' />
       <Layout>
         <Form>
           {!success && <span>
             <div className={'flex flex-wrap -mx-3 mb-6'}>
               <div className={'w-full md:w-1/2 px-3 mb-6 md:mb-0'}>
                 <label className={'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'} htmlFor='grid-first-name'>Seu nome</label>
-                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-first-name' type='text' placeholder='Jane' name='Nome' value={form.Nome} onChange={onChange} />
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
+                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-first-name' type='text' placeholder='Jane' name='Nome' value={form.Nome} onChange={handleChange} />
               </div>
               <div className={'w-full md:w-1/2 px-3 mb-6 md:mb-0'}>
                 <label className={'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'} htmlFor='grid-last-name'>Seu sobrenome</label>
-                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-last-name' type='text' placeholder='Doe' name='Sobrenome' value={form.Sobrenome} onChange={onChange} />
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
+                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-last-name' type='text' placeholder='Doe' name='Sobrenome' value={form.Sobrenome} onChange={handleChange} />
               </div>
             </div>
             <div className={'flex flex-wrap -mx-3 mb-6'}>
               <div className={'w-full md:w-1/2 px-3 mb-6 md:mb-0'}>
                 <label className={'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'} htmlFor='grid-email'>Email</label>
-                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-email' type='email' placeholder='seu@email.com' name='Email' value={form.Email} onChange={onChange} />
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
+                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-email' type='email' placeholder='seu@email.com' name='Email' value={form.Email} onChange={handleChange} />
               </div>
               <div className={'w-full md:w-1/2 px-3 mb-6 md:mb-0'}>
                 <label className={'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'} htmlFor='grid-whatsapp'><span className={'text-gray-600 text-xs italic'}>DDD</span> + WhatsApp</label>
-                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-whatsapp' type='phone' placeholder='99 999999999' name='WhatsApp' value={form.WhatsApp} onChange={onChange} />
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
+                <input className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} id='grid-whatsapp' type='phone' placeholder='99 999999999' name='WhatsApp' value={form.WhatsApp} onChange={handleChange} />
               </div>
             </div>
             <div className={'flex flex-wrap -mx-3 mb-6'}>
@@ -75,25 +72,23 @@ const Pesquisa = () => {
                     return (
                       <span className={'block w-1/6 tracking-wide text-center text-gray-700 text-xs font-bold mb-2'} key={id} htmlFor='grid-grade'>
                         {nota}<br />
-                        <input type='radio' name='Nota' value={nota} onChange={onChange} />
+                        <input type='radio' name='Nota' value={nota} onChange={handleChange} />
                       </span>
                     )
                   })}
                 </div>
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
               </div>
             </div>
             <div className={'flex flex-wrap -mx-3 mb-6'}>
               <div className={'w-full px-3'}>
                 <label className={'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'} htmlFor='grid-message'>Mensagem</label>
-                <textarea className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} rows='5' cols='50' id='grid-message' type='text-area' placeholder='Digite aqui' name='Mensagem' value={form.Mensagem} onChange={onChange} />
-                <p className={'text-gray-600 text-xs italic'}>Mensagem de validação</p>
+                <textarea className={'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'} rows='5' cols='50' id='grid-message' type='text-area' placeholder='Digite aqui' name='Mensagem' value={form.Mensagem} onChange={handleChange} />
               </div>
             </div>
             <div className={'flex flex-wrap -mx-3 mb-6'}>
               <div className={'w-full px-3'}>
                 <div className={'flex flex-row-reverse space-x-4 space-x-reverse'}>
-                  <button className={'text-white font-bold bg-red-600 hover:bg-red-700 rounded-full focus:outline-none focus:shadow-outline py-2 px-4'} type='button' onClick={send}>
+                  <button className={'text-white font-bold bg-red-600 hover:bg-red-700 rounded-full focus:outline-none focus:shadow-outline py-2 px-4'} type='submit' onClick={sendData}>
                     ENVIAR
                   </button>
                   <Link href='/'>
