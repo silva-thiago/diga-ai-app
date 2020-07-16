b0295a0647d5f3b710200e1a13c9d76c1803f9a8
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-16 15:12:02 -0300

    Melhoria estética e responsiva

--- components/header/menu-items.jsx
+++ components/header/menu-items.jsx
@@ -11,9 +11,9 @@ const MenuItems = () => {
   return (
     <React.Fragment>
       <div className={'lg:flex-grow'}>
-        <ul className={'w-auto sm:w-auto md:w-full lg:w-32 xl:w-3/4 lg:flex'}>
+        <ul className={'lg:flex lg:max-w-xl'}>
           {links.map(({ href, label }) => (
-            <li key={`${href}${label}`} className='text-gray-900 hover:text-red-600 hover:bg-gray-200 hover:shadow block rounded-full px-3 py-2 lg:flex-1 lg:inline-block lg:mt-0 lg:mx-4'>
+            <li key={`${href}${label}`} className='text-gray-900 hover:text-red-600 hover:bg-gray-200 hover:shadow block rounded-full px-4 py-2 lg:flex-1 lg:inline-block lg:mt-0 lg:mx-4'>
               <Link href={href}>
                 <a className='no-underline'>{label}</a>
               </Link>
