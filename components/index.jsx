8cfcea7f4fd2f3d23dfea14423df2af5af73a4d6
thiagosilva <thiago.silva@imd.ufrn.br>
2020-07-16 18:08:57 -0300

    Propriedade DOM corrigida

--- components/index.jsx
+++ components/index.jsx
@@ -15,10 +15,10 @@ const PageTitle = ({ title }) => {
         <meta name='google-site-verification' content='' />
         {/* Let browser know webapp is optimized for mobile */}
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-        <meta http-equiv='x-ua-compatible' content='ie=edge' />
-        <meta http-equiv='cache-control' content='max-age=31536000' />
-        <meta http-equiv='pragma' content='max-age=31536000' />
-        <meta http-equiv='refresh' content='3600;url=https://digaai.vercel.app/' />
+        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta httpEquiv='cache-control' content='max-age=31536000' />
+        <meta httpEquiv='pragma' content='max-age=31536000' />
+        <meta httpEquiv='refresh' content='3600;url=https://digaai.vercel.app/' />
         {/* Open Graph Markup: og */}
         <meta property='og:type' content='webapp' />
         <meta property='og:url' content='https://digaai.vercel.app/' />
